6cf775127186d33efce68c99c3f7b99e608f0198
Joshua Reibert <joshua.reibert@dlr.de>
2023-02-03 12:26:24 +0100

    Use point cloud layer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,27 @@
 <script>
 	import Map from "$lib/components/Map.svelte";
-	import { ScatterplotLayer } from "@deck.gl/layers";
+	import { COORDINATE_SYSTEM } from "@deck.gl/core";
+	import { PointCloudLayer } from "@deck.gl/layers";
 
 	let color = [0, 0, 0];
 
 	setInterval(() => {
 		color = [Math.random() * 255, Math.random() * 255, Math.random() * 255];
 	}, 1000);
+
+	$: layers = [
+		new PointCloudLayer({
+			id: "PointCloudLayer",
+			data: "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/pointcloud.json",
+			getColor: color,
+			getNormal: (d) => d.normal,
+			getPosition: (d) => d.position,
+			pointSize: 2,
+			coordinateOrigin: [-122.4, 37.74],
+			coordinateSystem: COORDINATE_SYSTEM.METER_OFFSETS,
+			pickable: false
+		})
+	];
 </script>
 
-<Map
-	zoom={15}
-	layers={[
-		new ScatterplotLayer({
-			data: [{ position: [0, 0], color, radius: 100 }],
-			getColor: (d) => d.color,
-			getRadius: (d) => d.radius
-		})
-	]}
-/>
+<Map longitude={-122.4} latitude={37.74} zoom={11} {layers} />
