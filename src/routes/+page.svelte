c94df6e3271345341478d324fbbaad77eadc9509
Joshua Reibert <joshua.reibert@dlr.de>
2023-02-03 12:49:24 +0100

    Use single points, add comments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,28 +5,30 @@
 	import { onMount } from "svelte";
 
 	let n = 9;
-	let visible = Array(n).fill(true);
+	const randomVisibility = () => Array.from({ length: n }, () => Math.random() > 0.2);
+	let visible = randomVisibility();
 
 	onMount(() => {
 		// run this function in regular intervals
 		const timer = setInterval(() => {
-			visible = Array.from({ length: n }, () => Math.random() > 0.5);
+			visible = randomVisibility();
 		}, 1000);
 
 		// clear the interval when this component is destroyed
 		return () => clearInterval(timer);
 	});
 
+	// reactively (re-)create the layers array
 	$: layers = visible.map(
 		(visible, index) =>
 			new PointCloudLayer({
 				id: `PointCloudLayer${index}`,
 				visible,
-				data: "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/pointcloud.json",
-				getColor: (d) => d.color,
-				getNormal: (d) => d.normal,
+				data: [{ position: [0, 0, 0] }],
+				getColor: [255, 0, 0],
 				getPosition: (d) => d.position,
-				pointSize: 2,
+				pointSize: 1000,
+				sizeUnits: "meters",
 				coordinateOrigin: [-122.6 + index * 0.05, 37.74],
 				coordinateSystem: COORDINATE_SYSTEM.METER_OFFSETS,
 				pickable: false
